7cfd5eab8ebd1eb9f4b9d7a6b15c12fd070af801
ratzat <157432830+ratzat@users.noreply.github.com>
2024-01-22 18:36:05 -0600

    im not stupid.py

--- links.py
+++ links.py
@@ -4,11 +4,11 @@
 
 # each line of code is a step in the program
 # the first value (the one before the ",") is the path to the directory
-# replace any "*" in the path with the path to the Lethal Company directory (copy path to plugins folder, EX: C:\Program Files (x86)\Steam\steamapps\common\Lethal Company\)
+# any "*" in the path is replaced with the path to the Lethal Company directory (EX: C:\Program Files (x86)\Steam\steamapps\common\Lethal Company)
 # the second value (the one after the ",") is either a link to a google drive folder or an instruction
 # if the value is "delete_existing_files", the program will delete all files in the directory in that step. This is recommended before downloading something to that folder (aka directory)
 
 instructions = [
-    ["*", "delete_existing_files"],
-    ["*", "https://drive.google.com/drive/folders/1rQFSuPN6yJ93KDOpy45J31OWcyDXZNal"]
+    ["*/Bepinex/plugins", "delete_existing_files"],
+    ["*/Bepinex/plugins", "https://drive.google.com/drive/folders/1rQFSuPN6yJ93KDOpy45J31OWcyDXZNal"]
 ]
