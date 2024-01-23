82f92118877cd9c7bd8a5c443d5f7acdb2138d18
CornHusker89 <137812325+CornHusker89@users.noreply.github.com>
2024-01-22 16:32:38 -0800

    Update links.py

--- links.py
+++ links.py
@@ -4,7 +4,7 @@
 
 # each line of code is a step in the program
 # the first value (the one before the ",") is the path to the directory
-# replace any "*" in the path with the path to the Lethal Company directory (copy path to plugins folder, EX: C:\Program Files (x86)\Steam\steamapps\common\Lethal Company/Bepinex/plugins)
+# replace any "*" in the path with the path to the Lethal Company directory (copy path to plugins folder, EX: C:\Program Files (x86)\Steam\steamapps\common\Lethal Company\)
 # the second value (the one after the ",") is either a link to a google drive folder or an instruction
 # if the value is "delete_existing_files", the program will delete all files in the directory in that step. This is recommended before downloading something to that folder (aka directory)
 
