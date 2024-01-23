0ac7214b95a6fc9d67394bec2f957203d0ccddb6
ratzat <157432830+ratzat@users.noreply.github.com>
2024-01-22 18:31:22 -0600

    fixed and refined oopsy.py

--- links.py
+++ links.py
@@ -4,11 +4,11 @@
 
 # each line of code is a step in the program
 # the first value (the one before the ",") is the path to the directory
-# any "*" in the path will be replaced with the path to the Lethal Company directory
+# replace any "*" in the path with the path to the Lethal Company directory (copy path to plugins folder, EX: C:\Program Files (x86)\Steam\steamapps\common\Lethal Company/Bepinex/plugins)
 # the second value (the one after the ",") is either a link to a google drive folder or an instruction
 # if the value is "delete_existing_files", the program will delete all files in the directory in that step. This is recommended before downloading something to that folder (aka directory)
 
 instructions = [
-    ["C:\Program Files (x86)\Steam\steamapps\common\Lethal Company/BepInEx/plugins", "delete_existing_files"],
-    ["C:\Program Files (x86)\Steam\steamapps\common\Lethal Company/BepInEx/plugins", "https://drive.google.com/drive/folders/1rQFSuPN6yJ93KDOpy45J31OWcyDXZNal"]
+    ["*", "delete_existing_files"],
+    ["*", "https://drive.google.com/drive/folders/1rQFSuPN6yJ93KDOpy45J31OWcyDXZNal"]
 ]
