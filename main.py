9273fd9ab39c25ebeca3ec4d119465998cf4638d
CornHusker89 <cornhusker889@gmail.com>
2024-01-27 22:47:12 -0800

    added final message

--- main.py
+++ main.py
@@ -79,6 +79,8 @@ try:
                 gdown.download_folder(url=instruction, output=path)
             except:
                 raise Exception(f"An error occured while downloading a folder with link {instruction} (make sure this is a valid and publicly accessable link). Error: {traceback.format_exc()}")
+            
+        input("Mods successfully installed. Press enter to exit")
                 
 except Exception as e:
     print("An error occured:")
